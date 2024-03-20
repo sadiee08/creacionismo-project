8c9cbace97287127342c86f2e0e6a278b958f73a
Abigail Sosa <61513075+sadiee08@users.noreply.github.com>
2024-03-20 13:48:41 -0600

    Update routes in App.js to include new pages

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import LoginPage from './components/auth/login/LoginPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { selectUsers } from './store/usersSlice.js';
 import { useSelector } from 'react-redux';
-import  Home from './components/home/index.jsx';
-
+import  Home from './pages/home/index.jsx';
+import  Catalogos from './pages/catalogos/index.jsx';
+import Pedidos from './pages/pedidos/index.jsx';
+import Configuracion from './pages/configuracion/index.jsx';
 
 function App() {
   const user = useSelector(selectUsers);
@@ -15,6 +17,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />}  />
+          <Route path="/menu-catalogos" element={<Catalogos />} />
+          <Route path="/pedidos" element={<Pedidos />} />
+          <Route path="/configuracion" element={<Configuracion />} />
         </Routes>
       </BrowserRouter> 
       :
