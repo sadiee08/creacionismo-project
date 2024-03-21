91f66162a0a8dcb4cc298696f536e3119b6e7796
Abigail Sosa <61513075+sadiee08@users.noreply.github.com>
2024-03-21 12:42:27 -0600

    Add MenuCatalogo component and styles, update catalogos page

--- src/pages/catalogos/index.jsx
+++ src/pages/catalogos/index.jsx
@@ -1,15 +1,31 @@
 import Menu from "../../components/menu/menu";
 import Header from "../../components/header";
 import "./styles.css";
+import MenuCatalogo from "../../components/common/menu-catalogo/menu-catalogo";
+import Pedestales from "../../assets/images/pedestales.jpg"
+import Aire from "../../assets/images/aire.jpg"
+import Ceramica from "../../assets/images/ceramica.jpg"
+import Concreto from "../../assets/images/concreto.jpg"
+import Tierra from "../../assets/images/tierra.jpg"
 
 const Catalogos = () => {
+
     return (
         <>
             <div>
-                <Header/>
+                <Header />
                 <div className="display-menu">
                     <Menu />
-                    <h1>hola desde catalogos</h1>
+                    <div className="width-menu">
+                        <div className="menu-align">
+                            <MenuCatalogo imagen={Pedestales} texto={"Pedestales"} />
+                            <MenuCatalogo imagen={Aire} texto={"Maceta Aire"} />
+                            <MenuCatalogo imagen={Ceramica} texto={"Maceta Ceramica"} />
+                            <MenuCatalogo imagen={Concreto} texto={"Maceta Concreto"} />
+                            <MenuCatalogo imagen={Tierra} texto={"Maceta Tierra"} />
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
 
