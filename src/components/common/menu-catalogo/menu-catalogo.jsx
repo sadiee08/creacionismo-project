f39056919742317c93d549e0a4e86243982b9f7b
Abigail Sosa <61513075+sadiee08@users.noreply.github.com>
2024-04-03 09:21:16 -0600

    Update MenuCatalogo component to use NavLink instead of anchor tag

--- src/components/common/menu-catalogo/menu-catalogo.jsx
+++ src/components/common/menu-catalogo/menu-catalogo.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 import "./styles.css";
 
-const MenuCatalogo = ({ imagen, texto }) => {
+const MenuCatalogo = ({ imagen, texto, url }) => {
 
+    const linkName = "/menu-catalogos/" + url;
 
     return (
         <>
             <div className="cuadro">
-                <a href="#">
+                <NavLink to={linkName}>
                     <div className="contenedor-imagen">
                         <img src={imagen} alt={texto} />
                         <div className="texto-imagen">{texto}</div>
                     </div>
-                </a>
+                </NavLink>
             </div>
 
         </>
