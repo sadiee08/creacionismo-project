6742b1a07cebf70fdbdc15fc927f5c23d5e8de7e
Abigail Sosa <61513075+sadiee08@users.noreply.github.com>
2024-04-03 09:21:30 -0600

    Add TotalCount component and its functionality to display total number of products

--- src/components/common/total-count/index.jsx
+++ src/components/common/total-count/index.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./styles.css";
 
-const TotalCount = ({ imagen, texto }) => {
+import { db } from '../../../firebase/config';
+import { collection, getDocs } from 'firebase/firestore';
+
+const TotalCount = ({ imagen, texto, producto, totalProdutos }) => {
+    const [total, setTotal] = useState(0);
+    console.log(producto)
+
+    useEffect(() => {
+        const fetchTotal = async () => {
+            const productCollection = collection(db, producto);
+            const productSnapshot = await getDocs(productCollection);
+            setTotal(productSnapshot.size);
+        };
+
+        fetchTotal();
+    }, []);
+
+    let totalAux = totalProdutos ? totalProdutos : total;
+
 
     return (
         <>
@@ -9,7 +27,7 @@ const TotalCount = ({ imagen, texto }) => {
                 <img src={imagen} alt={texto} />
                 <div>
                     <p className="p-margin-bottom">{texto}</p>
-                    <p className="p-margin-top">10</p>
+                    <p className="p-margin-top">{totalAux}</p>
                 </div>
             </div>
         </>
