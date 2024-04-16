c9e56bf216325d3925bdeae9b71333a3abf7e3df
Abigail Sosa <61513075+sadiee08@users.noreply.github.com>
2024-04-15 21:20:08 -0600

    feat: Add lazy loading for images in catalog pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import LoginPage from './components/auth/login/LoginPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { selectUsers } from './store/usersSlice.js';
 import { useSelector } from 'react-redux';
-import  Home from './pages/home/index.jsx';
-import  Catalogos from './pages/catalogos/index.jsx';
+import Home from './pages/home/index.jsx';
+import Catalogos from './pages/catalogos/index.jsx';
 import Pedidos from './pages/pedidos/index.jsx';
 import Configuracion from './pages/configuracion/index.jsx';
 import Producto from './pages/producto/producto.jsx';
+
+import Pedestales from "../src/assets/images/pedestales.jpg"
+import Aire from "../src/assets/images/aire.jpg"
+import Ceramica from "../src/assets/images/ceramica.jpg"
+import Concreto from "../src/assets/images/concreto.jpg"
+import Tierra from "../src/assets/images/tierra.jpg"
 
 function App() {
   const user = useSelector(selectUsers);
@@ -24,11 +30,11 @@ function App() {
           <Route path="/configuracion" element={<Configuracion />} />
 
           {/* menu-catalogos */}
-          <Route path="/menu-catalogos/pedestales" element={<Producto producto={"pedestales"} titulo={"Pedestales"} />} />
-          <Route path="/menu-catalogos/maceta-aire" element={<Producto producto={"maceta-aire"} titulo={"Macetas Aire"}/>} />
-          <Route path="/menu-catalogos/maceta-ceramica" element={<Producto producto={"maceta-ceramica"} titulo={"Macetas Cerámica"}/>} />
-          <Route path="/menu-catalogos/maceta-concreto" element={<Producto producto={"maceta-concreto"} titulo={"Macetas Concreto"}/>} />
-          <Route path="/menu-catalogos/maceta-tierra" element={<Producto producto={"maceta-tierra"} titulo={"Macetas Tierra"}/>} />
+          <Route path="/menu-catalogos/pedestales" element={<Producto producto={"pedestales"} titulo={"Pedestales"} img={Pedestales} />} />
+          <Route path="/menu-catalogos/maceta-aire" element={<Producto producto={"maceta-aire"} titulo={"Macetas Aire"} img={Aire} />} />
+          <Route path="/menu-catalogos/maceta-ceramica" element={<Producto producto={"maceta-ceramica"} titulo={"Macetas Cerámica"} img={Ceramica} />} />
+          <Route path="/menu-catalogos/maceta-concreto" element={<Producto producto={"maceta-concreto"} titulo={"Macetas Concreto"} img={Concreto} />} />
+          <Route path="/menu-catalogos/maceta-tierra" element={<Producto producto={"maceta-tierra"} titulo={"Macetas Tierra"} img={Tierra} />} />
 
         </Routes>
       </BrowserRouter> 
