faac4820d0da929e7cd02bca080e827da8165a96
Abigail Sosa <61513075+sadiee08@users.noreply.github.com>
2024-04-23 22:49:57 -0600

    feat: Add password reset functionality to Configuracion page

--- src/pages/configuracion/index.jsx
+++ src/pages/configuracion/index.jsx
@@ -2,14 +2,31 @@ import Menu from "../../components/menu/menu";
 import Header from "../../components/header";
 import "./styles.css";
 
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../../firebase/config";
+
 const Configuracion = () => {
+
+    const handlePasswordReset = () => {
+        const email = prompt("Ingresa tu correo electrónico para restablecer la contraseña");
+        sendPasswordResetEmail(auth, email);
+        alert("Se ha enviado un correo electrónico a tu bandeja de entrada con las instrucciones para restablecer la contraseña");
+    }
+    
     return (
         <>
-            <div>
+            <div className="con">
                 <Header/>
                 <div className="display-menu">
                     <Menu />
-                    <h1>hola desde Configuración</h1>
+                    <div className="container-config">
+                        <h2>Configuración</h2>
+                        <div>
+                            <p>¿Deseas cambiar la contraseña?</p>
+                            <button onClick={handlePasswordReset} className="btn-password">Cambiar contraseña</button>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
 
